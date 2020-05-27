d7de133030414a0953ad5f7e9cf625bc7c799865
ramon96 <rfm1@live.nl>
2020-05-27 13:37:36 +0200

    removed vue, added nuxt

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
   parserOptions: {
-    ecmaVersion: 2020
+    parser: 'babel-eslint'
   },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  // add your custom rules here
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'nuxt/no-cjs-in-config': 'off'
   }
 }
