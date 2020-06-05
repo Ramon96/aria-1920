987223ba87b786267ab2ed91508f0a444507952d
ramon96 <rfm1@live.nl>
2020-06-05 16:10:20 +0200

    added the youtube api

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import express from 'express'
 
 const spotify = require('./spotify/spotify.js')
 const instagram = require('./instagram/instagram.js')
+const youtube = require('./youtube/youtube.js')
 const musicBrainz = require('./musicbrainz/musicbrainz.js')
 const newsApi = require('./NewsApi/NewsApi.js')
 // const twitter = require('./twitter/twitter.js')
@@ -14,6 +15,7 @@ app.use(express.json())
 
 
 app.use(spotify)
+app.use(youtube)
 app.use(musicBrainz)
 app.use(newsApi)
 app.use(instagram)
