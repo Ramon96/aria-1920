ab3308fc69acdd67c2aefd8371e30e24ba15f303
ramon96 <rfm1@live.nl>
2020-06-12 12:07:22 +0200

    more logging

--- api/instagram/instagram.js
+++ api/instagram/instagram.js
@@ -23,6 +23,7 @@ async function getUserPage(handle){
 
     // const data = await instagramJson.json()
     // console.log(data)
+    console.log(instagramJson)
     return getShortCodes(instagramJson.graphql.user.edge_owner_to_timeline_media.edges)
 }
 
