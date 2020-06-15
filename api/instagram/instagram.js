f129e63032307e69ef649f554b72f4ffc5019bde
ramon96 <rfm1@live.nl>
2020-06-15 18:26:23 +0200

    trying a scraper provided by gijs

--- api/instagram/instagram.js
+++ api/instagram/instagram.js
@@ -1,32 +1,58 @@
 const express = require('express');
 const router = express.Router();
 
-const ig = require('instagram-scraping');
-const delay = require('delay');
+const fetch = require('node-fetch');
+const Nanogram = require('nanogram.js');
 
-async function getMediaId(handle){
-    await delay(getRandomArbitrary(1000,2000));
-    return  ig.scrapeTag('eminem')
-        .then(  result => {
-        return result.medias.map( post => {
-            return post.shortcode
-        })
-      });
+(function () {
+  if (typeof globalThis === 'object') return;
+  Object.defineProperty(Object.prototype, '__magic__', {
+    get: function () {
+      return this;
+    },
+    configurable: true
+  });
+  __magic__.globalThis = __magic__;
+  delete Object.prototype.__magic__;
+}());
+
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch;
 }
+const instagramParser = new Nanogram();
 
-router.get('/instagram/recent/:url', async (req,res) =>{
-    try{ 
+
+
+async function getMediaId(handle) {
+    return await instagramParser.getMediaByUsername(handle).then((media) => {
+            const {
+              profile: {
+                  edge_owner_to_timeline_media: {
+                    edges
+                  }
+              }
+            } = media;
+
+        return getShortCodes(edges)
+
+      })
+    }
+
+    function getShortCodes(edges) {
+      return edges.map(edge => {
+        return edge.node.shortcode
+      })
+    }
+
+    router.get('/instagram/recent/:url', async (req, res) => {
+      try {
         // const recentPosts = await getUserPage(req.params.url)
         const recentPosts = await getMediaId(req.params.url)
         res.send(recentPosts)
-    }
-    catch(err){
+      } catch (err) {
         console.log(err)
-    }
-})
+      }
+    })
 
-function getRandomArbitrary(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
 
-module.exports = router;
+    module.exports = router;
